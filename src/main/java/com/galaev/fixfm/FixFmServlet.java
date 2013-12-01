f9c9119e9597ce79ca0e9a9dc127ca18fe2802d5
anton <wgalaev@gmail.com>
2013-12-01 06:31:05 +0400

    concurrency fixed

--- src/main/java/com/galaev/fixfm/FixFmServlet.java
+++ src/main/java/com/galaev/fixfm/FixFmServlet.java
@@ -17,28 +17,33 @@ public class FixFmServlet extends HttpServlet {
 
     private static Logger logger = LoggerFactory.getLogger(FixFmServlet.class);
     private String token;
-    private Object lock = new Object();
+    private final Object lock = new Object();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         token = req.getParameter("token");
         logger.info("Got the token");
-        lock.notify();
+        synchronized (lock) {
+            lock.notifyAll();
+        }
+        resp.setStatus(HttpServletResponse.SC_OK);
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        // create app and extract all parameters
-        FixFmApp fixFmApp = new FixFmApp();
-        fixFmApp.extractParams(req);
         // wait for token acquiring
         while (token == null) {
             try {
-                lock.wait();
+                synchronized (lock) {
+                    lock.wait();
+                }
             } catch (InterruptedException e) {
                 e.printStackTrace();
             }
         }
+        // create app and extract all parameters
+        FixFmApp fixFmApp = new FixFmApp();
+        fixFmApp.extractParams(req);
         fixFmApp.setToken(token);
         logger.info("Token acquired and set");
         // process the request
